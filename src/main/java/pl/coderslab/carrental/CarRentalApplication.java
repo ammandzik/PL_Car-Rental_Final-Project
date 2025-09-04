3d27bde0d38d2cd09c6cce65525e115b1e7769f2
apreisne <apreisne@visa.com>
2025-09-04 15:46:38 +0200

    configured app start within docker

--- src/main/java/pl/coderslab/carrental/CarRentalApplication.java
+++ src/main/java/pl/coderslab/carrental/CarRentalApplication.java
@@ -12,6 +12,9 @@ public class CarRentalApplication {
         System.setProperty("MYSQL_USER", EnvLoader.getEnv("MYSQL_USER"));
         System.setProperty("MYSQL_PASSWORD", EnvLoader.getEnv("MYSQL_PASSWORD"));
         System.setProperty("MYSQL_DATABASE", EnvLoader.getEnv("MYSQL_DATABASE"));
+        System.setProperty("SPRING_DATASOURCE_URL", EnvLoader.getEnv("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USER", EnvLoader.getEnv("SPRING_DATASOURCE_USER"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", EnvLoader.getEnv("SPRING_DATASOURCE_PASSWORD"));
         System.setProperty("DB_HOST", EnvLoader.getEnv("DB_HOST"));
         System.setProperty("DB_PORT", EnvLoader.getEnv("DB_PORT"));
 
